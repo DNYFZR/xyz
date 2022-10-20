277f686a81283e1239abf4d2325ae8a6d9c364a1
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-10-20 21:09:51 +0100

    Updated asset_registry.py

--- src/asset_registry.py
+++ src/asset_registry.py
@@ -6,23 +6,20 @@ from dagster import AssetIn, MetadataValue, asset, file_relative_path
 PROJECT_NAME = "dbt_project"
 DB_NAME = "dagster.duckdb"
 
-# Asset Sources
-CUSTOMERS_RAW = "https://docs.dagster.io/assets/customers.csv"
-ORDERS_RAW = "https://docs.dagster.io/assets/orders.csv"
-
-# Output Schema
-OUTPUT_NAME = "OrderCount.html"
+# Assets
+CUSTOMERS_RAW_INPUT = "https://docs.dagster.io/assets/customers.csv"
+ORDERS_RAW_INPUT = "https://docs.dagster.io/assets/orders.csv"
+CUSTOMERS_OUTPUT = "OrderCount.html"
 
 # Asset Config
-
 class dagster_input_assets:
     @asset(key_prefix=[PROJECT_NAME], group_name="staging")
     def customers_raw() -> pd.DataFrame:
-        return pd.read_csv(CUSTOMERS_RAW)
+        return pd.read_csv(CUSTOMERS_RAW_INPUT)
 
     @asset(key_prefix=[PROJECT_NAME], group_name="staging")
     def orders_raw() -> pd.DataFrame:
-        return pd.read_csv(ORDERS_RAW)
+        return pd.read_csv(ORDERS_RAW_INPUT)
 
 
 class dagster_output_assets:
@@ -31,7 +28,7 @@ class dagster_output_assets:
         fig = px.histogram(customers, x = 'number_of_orders')
         fig.update_layout(bargap = 0.25)
 
-        chart_path = file_relative_path(__file__, f"../output/{OUTPUT_NAME}")
+        chart_path = file_relative_path(__file__, f"../output/{CUSTOMERS_OUTPUT}")
         fig.write_html(chart_path, auto_open=True)
 
         context.add_output_metadata({"plot_url": MetadataValue.url(f"file://{chart_path}")})
